c3f84daf840e108dcfecf2147760184d412681c3
Gabriel Altino <148129785+Gabriellenda29@users.noreply.github.com>
2025-04-14 17:36:35 -0300

    Add files via upload

---
+++ atvFinalRicardo/src/FilaSimplesAtv.java
@@ -0,0 +1,307 @@
+public class FilaSimplesAtv implements IEstruturaSimples {
+
+    private int tamanho;
+    private Integer [] fila;
+
+    public FilaSimplesAtv(int tamanho) {
+        this.tamanho = tamanho;
+        fila = new Integer[tamanho];
+        System.out.println("Fila criada.");
+    }
+
+    @Override
+
+    //certo
+    public void inserirElemento(Object valor) {
+        for (int i = 0; i < tamanho; i++) {
+            if(this.fila[i] == null) {
+                this.fila[i] = (Integer) valor;
+                //System.out.println("Valor inserido["+i+"]: "+ valor);
+                return;
+            }
+        }
+        System.out.println("Valor " + valor + " não pode ser inserido. Lista cheia");
+    }
+
+    @Override
+    //certo
+    public void inserirElementoIndice(Object valor, int indice) {
+        for(int i = 0; i < tamanho; i++) {
+            if(this.fila[indice] == null) {
+                this.fila[indice] = (Integer) valor;
+                System.out.println("Valor " + valor + " inserido no índice: " + indice );
+                return;
+            }
+        }
+
+        System.out.println("Indice já preenchido: " + indice);
+    }
+
+    @Override
+    //certo
+    public void inserirSequencia(Object valor) {
+        if(estaCheia()) {
+            System.out.println("Valor " + valor + " não pode ser inserido. Fila cheia");
+            return;
+        }
+        for (int i = 0; i < tamanho; i++) {
+            if (this.fila[i] == null) {
+                this.fila[i] = (Integer) valor;
+                System.out.println("Valor inserido[" + i + "]: " + valor);
+                return;
+            }
+        }
+    }
+
+    @Override
+    //certo
+    public boolean removerElemento() {
+        for (int i = 0; i < tamanho; i++) {
+            if(this.fila[i] != null) {
+                System.out.println("Removendo primeiro elemento da fila: " + this.fila[i]);
+                this.fila[i] = null;
+                return true;
+            }
+        }
+        System.out.println("Nenhum elemento removido");
+        return false;
+    }
+
+    @Override
+    //certo
+    public Object removerIndice(int indice) {
+        if(indice < 0 || indice > tamanho - 1){
+            System.out.println("Indice inválido");
+            return null;
+        }
+
+        if (this.fila[indice] != null) {
+            this.fila[indice] = null;
+            System.out.println("Índice removido[" + indice + "]: " + this.fila[indice]);
+        } else {
+            System.out.println("Índice vazio");
+        }
+
+       return null;
+    }
+
+    @Override
+    public void removerSequencia(Object valor) {
+        if (estaVazia()) {
+            System.out.println("Nenhum elemento para remover");
+            return;
+        }
+
+        for (int i = 0; i < tamanho; i++) {
+            if (this.fila[i] != null && this.fila[i].equals(valor)) {
+                this.fila[i] = null;
+                System.out.println("Valor removido[" + i + "]: " + valor);
+                System.out.println("Fila Agora:");
+                exibir();
+                break;
+            }else{
+                System.out.println("Valor inválido");
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    //certo
+    public void removerTodasOcorrencias(Object valor) {
+
+        for (int i = 0; i < tamanho; i++) {
+            if(this.fila[i] == valor && this.fila[i] != null) {
+                this.fila[i] = null;
+            }
+        }
+
+        if (!buscarElemento(valor)) {
+            System.out.println("Nenhum elemento para remover");
+        }
+
+    }
+
+    @Override
+    //certo
+    public boolean estaCheia() {
+        for (int i = 0; i < tamanho; i++) {
+            if (this.fila[i] == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    //certo
+    public boolean estaVazia() {
+        for (int i = 0; i < tamanho; i++) {
+            if (this.fila[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    //certo
+    public boolean buscarElemento(Object valor) {
+        for (int i = 0; i < tamanho; i++) {
+            if(this.fila[i] == valor) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    //certo
+    public Object buscarElementoIndice(int indice) {
+        if(indice >=0 && indice < tamanho) {
+                if (this.fila[indice] != null) {
+                    System.out.println("Elemento armazenado no indice ["+ indice +"]:" + this.fila[indice]);
+                    return this.fila[indice];
+                }else{
+                    System.out.println("Indice vazio");
+                }
+        }
+        else{
+            System.out.println("Indice inválido");
+            return null;
+        }
+
+        return null;
+    }
+
+    @Override
+    //certo
+    public void ordenarCrescente() {
+        int aux;
+
+        for (int i = 0; i < tamanho; i++) {
+            if((i+1) < tamanho) {
+                if (this.fila[i] > this.fila[i + 1]) {
+                    aux = this.fila[i];
+                    this.fila[i] = this.fila[i + 1];
+                    this.fila[i + 1] = aux;
+                    ordenarCrescente();
+                }
+            }
+        }
+    }
+
+    @Override
+    //certo
+    public void ordenarDecrescente() {
+        int aux;
+
+        for (int i = tamanho - 1; i >= 0; i--) {
+            if((i+1) < tamanho) {
+                if (this.fila[i] < this.fila[i + 1]) {
+                    aux = this.fila[i];
+                    this.fila[i] = this.fila[i + 1];
+                    this.fila[i + 1] = aux;
+                    ordenarDecrescente();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    //certo
+    public int quantidadeElementos() {
+        int cont = 0;
+        for (int i = 0; i < tamanho; i++) {
+            if(this.fila[i] != null) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+    @Override
+    public void dobrarCapacidade() {
+        int tamanhox2 = this.tamanho*2;
+        Integer[] fila2 = new Integer[tamanhox2];
+
+        if(estaVazia()) {
+            System.out.println("Não é possóvel aumentar a capacidade. Fila vazia");
+        }else {
+            for (int i = 0; i < tamanho; i++) {
+                fila2[i] = this.fila[i];
+            }
+        }
+
+        this.fila = fila2;
+        this.tamanho = tamanhox2;
+    }
+
+    @Override
+    //certo
+    public void editarElemento(Object elementoAntigo, Object elementoNovo) {
+        if(buscarElemento(elementoAntigo)) {
+            for (int i = 0; i < tamanho; i++) {
+                if(this.fila[i] == elementoAntigo) {
+                    fila[i] = (Integer) elementoNovo;
+                    System.out.println("Elemento na posição [" + i + "] alterado para: " + elementoNovo);
+                }
+            }
+        }
+        else{
+            System.out.println("Elemento nao encontrado");
+        }
+
+    }
+
+    @Override
+    //certo
+    public void limpar() {
+        if(this.fila[0] != null) {
+            for (int i = 0; i < tamanho; i++) {
+                if(this.fila[i] != null) {
+                    this.fila[i] = null;
+                }
+            }
+        }
+        else{
+            System.out.println("Fila já está vazia");
+        }
+
+    }
+
+    @Override
+    //certo
+    public void exibir() {
+        System.out.println("Exibindo elementos da fila.");
+        for (int i = 0; i < this.tamanho; i++) {
+            System.out.println("Fila[" + (i) + "] = " + this.fila[i]);
+        }
+    }
+
+    @Override
+    //certo
+    public Object obterPrimeiroElemento() {
+        for (int i = 0; i < tamanho; i++) {
+            if (fila[i] != null) {
+                System.out.println("Primeiro elemento não-nulo da fila: " + fila[i]);
+                return fila[i];
+            }
+        }
+        return "Lista vazia";
+    }
+
+    @Override
+    //certo
+    public Object obterUltimoElemento() {
+        for (int i = tamanho - 1; i >= 0; i--) {
+            if (fila[i] != null) {
+                System.out.println("Ultimo elemento não-nulo da fila: " + fila[i]);
+                return fila[i];
+            }
+        }
+        return "Fila vazia";
+    }
+}
---
+++ atvFinalRicardo/src/MainFilaSimples.java
@@ -0,0 +1,157 @@
+import java.util.Scanner;
+
+public class MainFilaSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        FilaSimplesAtv fila = new FilaSimplesAtv(5);
+
+        int opcao,vlr;
+
+
+        do {
+            System.out.println("\n*****SAFEFIL-|-|-MENU*****");
+            System.out.println("1. Inserir Elemento");
+            System.out.println("2. Inserir Elemento em Índice");
+            System.out.println("3. Inserir Sequência");
+            System.out.println("4. Remover Primeiro Elemento");
+            System.out.println("5. Remover por Índice");
+            System.out.println("6. Remover Primeira Ocorrência");
+            System.out.println("7. Remover Todas Ocorrências");
+            System.out.println("8. Buscar Elemento");
+            System.out.println("9. Buscar por Índice");
+            System.out.println("10. Ordenar Crescente");
+            System.out.println("11. Ordenar Decrescente");
+            System.out.println("12. Verificar se está cheia");
+            System.out.println("13. Verificar se está vazia");
+            System.out.println("14. Quantidade de Elementos");
+            System.out.println("15. Dobrar Capacidade");
+            System.out.println("16. Editar Elemento");
+            System.out.println("17. Limpar Fila");
+            System.out.println("18. Exibir Fila");
+            System.out.println("19. Obter Primeiro Elemento");
+            System.out.println("20. Obter Último Elemento");
+            System.out.println("21. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o valor: ");
+                    vlr = scanner.nextInt();
+                    fila.inserirElemento(vlr);
+                    scanner.nextLine();
+                    break;
+                case 2:
+                    System.out.print("Digite o valor: ");
+                    int valor = scanner.nextInt();
+                    System.out.print("Digite o índice: ");
+                    int indice = scanner.nextInt();
+                    scanner.nextLine();
+                    fila.inserirElementoIndice(valor, indice);
+                    break;
+                case 3:
+                    System.out.print("Digite o valor: ");
+                    vlr = scanner.nextInt();
+                    fila.inserirSequencia(vlr);
+                    scanner.nextLine();
+                    break;
+                case 4:
+                    fila.removerElemento();
+                    break;
+                case 5:
+                    System.out.print("Digite o índice: ");
+                    vlr = scanner.nextInt();
+                    fila.removerIndice(vlr);
+                    scanner.nextLine();
+                    break;
+                case 6:
+                    System.out.print("Digite o valor: ");
+                    vlr = scanner.nextInt();
+                    fila.removerSequencia(vlr);
+                    scanner.nextLine();
+                    break;
+                case 7:
+                    System.out.print("Digite o valor: ");
+                    vlr = scanner.nextInt();
+                    fila.removerTodasOcorrencias(vlr);
+                    scanner.nextLine();
+                    break;
+                case 8:
+                    System.out.print("Digite o valor: ");
+                    vlr = scanner.nextInt();
+                    System.out.println(fila.buscarElemento(vlr));
+                    scanner.nextLine();
+                    break;
+                case 9:
+                    System.out.print("Digite o índice: ");
+                    vlr = scanner.nextInt();
+                    fila.buscarElementoIndice(vlr);
+                    scanner.nextLine();
+                    break;
+                case 10:
+                    fila.ordenarCrescente();
+                    break;
+                case 11:
+                    fila.ordenarDecrescente();
+                    break;
+                case 12:
+                    System.out.println("Fila cheia? ");
+                    if(fila.estaCheia()){
+                        System.out.println("Sim, a fila está cheia");
+                    }else{
+                        System.out.println("Não, a fila tem indíces livres");
+                    }
+                    break;
+                case 13:
+                    System.out.print("Fila vazia? ");
+                    if (fila.estaVazia()) {
+                        System.out.print("SIm, a fila está vazia");
+                    }else{
+                        System.out.print("Não, a fila não está vazia");
+                    }
+                    break;
+                case 14:
+                    System.out.println("Quantidade de elementos: " + fila.quantidadeElementos());
+                    break;
+                case 15:
+                    fila.dobrarCapacidade();
+                    break;
+                case 16:
+                    System.out.print("Digite o valor antigo: ");
+                    int antigo = scanner.nextInt();
+                    System.out.print("Digite o valor novo: ");
+                    int novo = scanner.nextInt();
+                    scanner.nextLine();
+                    fila.editarElemento(antigo, novo);
+                    break;
+                case 17:
+                    fila.limpar();
+                    break;
+                case 18:
+                    fila.exibir();
+                    break;
+                case 19:
+                    fila.obterPrimeiroElemento();
+                    break;
+                case 20:
+                    fila.obterUltimoElemento();
+                    break;
+                case 21:
+                    System.out.println("Safefil diz adeus");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+
+            if (opcao != 21) {
+                System.out.println("\n<ENTER>");
+                scanner.nextLine();
+            }
+
+        } while (opcao != 21);
+
+        scanner.close();
+    }
+}
